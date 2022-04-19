9cff6c1f30dbed3f13e8f54df62504e745479c0a
DMITRII <jd2344430@gmail.com>
2022-04-19 18:15:20 +0500

    Java2 Homework8

--- src/main/java/com/example/chatjavafx/UIClient.java
+++ src/main/java/com/example/chatjavafx/UIClient.java
@@ -8,6 +8,8 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class UIClient extends Application {
+
+
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(UIClient.class.getResource("client-ui.fxml"));
@@ -20,4 +22,6 @@ public class UIClient extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
